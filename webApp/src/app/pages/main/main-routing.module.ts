94c35e6ee45fac0d67015971ede077b8b985b2f5
panjie <3792535@qq.com>
2018-11-08 16:52:53 +0800

    菜单增加链接

--- webApp/src/app/pages/main/main-routing.module.ts
+++ webApp/src/app/pages/main/main-routing.module.ts
@@ -2,11 +2,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
 import { AppComponent } from './app/app.component';
+import { LogComponent } from './log/log.component';
 
 const routes: Routes = [
-	{ path: 'main', component: MainComponent, children: [
-		{path: 'app', component: AppComponent }
-	] }
+	{
+		path: 'main', component: MainComponent, children: [
+			{ path: 'app', component: AppComponent },
+			{ path: 'log', component: LogComponent }
+		]
+	}
 ];
 
 @NgModule({
