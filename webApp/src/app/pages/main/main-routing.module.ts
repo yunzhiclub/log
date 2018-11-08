891d4525edde0d6254cf5d60645654fe3fcdb8eb
panjie <3792535@qq.com>
2018-11-08 16:46:34 +0800

    add app

--- webApp/src/app/pages/main/main-routing.module.ts
+++ webApp/src/app/pages/main/main-routing.module.ts
@@ -1,9 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
+import { AppComponent } from './app/app.component';
 
 const routes: Routes = [
-	{ path: 'main', component: MainComponent }
+	{ path: 'main', component: MainComponent, children: [
+		{path: 'app', component: AppComponent }
+	] }
 ];
 
 @NgModule({
--- webApp/src/app/pages/main/main.module.ts
+++ webApp/src/app/pages/main/main.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { MainRouteModule } from './main-routing.module';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { AppComponent } from './app/app.component';
 
 // 模块声明
 
 @NgModule({
 	// 该模块的组成的组件声明
 	declarations: [
-		MainComponent
+		MainComponent,
+		AppComponent
 	],
 	// 该模块的依赖声明（在组件中，不需要声明依赖）
 	imports: [
