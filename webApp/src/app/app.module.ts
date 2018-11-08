eb2bb73da1f3eb6d7221ed52e74ca6e5f3f211f6
panjie <3792535@qq.com>
2018-11-08 16:43:06 +0800

    add main

--- webApp/src/app/app.module.ts
+++ webApp/src/app/app.module.ts
@@ -9,15 +9,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MainModule } from './pages/main/main.module';
+
+// 模块声明:
 
 registerLocaleData(zh);
 
 @NgModule({
+  // 声明自己的拥有的组件：即本模块是由哪几个组件组成的
   declarations: [
     AppComponent,
     LoginComponent
   ],
+  // 声明需要的第三方模块
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,9 +30,12 @@ registerLocaleData(zh);
     FormsModule,
     HttpClientModule,
     NgZorroAntdModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MainModule
   ],
+  // 声明provider
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  // 声明启动组件
   bootstrap: [AppComponent]
 })
 export class AppModule { }
