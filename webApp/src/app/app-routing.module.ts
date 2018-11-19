ad6919f2895d01330f034213572bb8c97b08a154
poshichao <1648907448@qq.com>
2018-11-19 11:46:14 +0800

    登录跳转

--- webApp/src/app/app-routing.module.ts
+++ webApp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
+    { path: 'main',  component: MainComponent},
 	{ path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
