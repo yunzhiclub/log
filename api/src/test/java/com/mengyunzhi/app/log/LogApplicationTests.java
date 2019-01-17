22450c15343b3544cc12083779f1dc89fd699d2b
panjie <3792535@qq.com>
2019-01-17 16:01:12 +0800

    init

---
+++ api/src/test/java/com/mengyunzhi/app/log/LogApplicationTests.java
@@ -0,0 +1,17 @@
+package com.mengyunzhi.app.log;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
