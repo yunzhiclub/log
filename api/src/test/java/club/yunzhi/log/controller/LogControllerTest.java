20bfa92a0043c44682ca627cf52d414168724def
panjie <3792535@qq.com>
2019-01-18 16:51:09 +0800

    增加事务

--- api/src/test/java/club/yunzhi/log/controller/LogControllerTest.java
+++ api/src/test/java/club/yunzhi/log/controller/LogControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
 
 /**
@@ -20,6 +21,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
+@Transactional
 public class LogControllerTest {
     @Autowired private ClientService clientService;
     @Autowired
