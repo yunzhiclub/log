a7b00e77b0feb396e7cdd5e9d02d978cb138af86
panjie <3792535@qq.com>
2019-01-26 12:57:25 +0800

    git rm target and 重新整理验证信息

---
+++ api/src/main/java/club/yunzhi/log/entity/User.java
@@ -0,0 +1,75 @@
+package club.yunzhi.log.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Calendar;
+
+/**
+ * @author panjie
+ * 用户
+ */
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 6903403699983360575L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name = "";
+    private final String password = "";
+    private String token = "";
+
+    private String email = "";
+    private Calendar time = Calendar.getInstance();
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+    public void setToken(final String token) {
+        this.token = token;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public Calendar getTime() {
+        return this.time;
+    }
+
+    public void setTime(final Calendar time) {
+        this.time = time;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+}
