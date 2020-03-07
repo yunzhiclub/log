14c55a3d1983b903ae7cbb396e68df341dc436e2
xingyannian <xingyannian@163.com>
2020-03-07 16:22:09 +0800

    完成客户端管理初始化

--- newWebApp/src/app/app.component.spec.ts
+++ newWebApp/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import {LeftnavComponent} from './leftnav/leftnav.component';
+import { LeftnavComponent } from './leftnav/leftnav.component';
 import {HeadnavComponent} from './headnav/headnav.component';
 
 describe('AppComponent', () => {
