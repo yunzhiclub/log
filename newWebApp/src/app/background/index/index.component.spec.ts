74ae61723a41e762e913ab466c65bc11d62ebeeb
lyx <1098659745@qq.com>
2020-03-17 18:11:46 +0800

    前台：后台日志管理_M层开发

--- newWebApp/src/app/background/index/index.component.spec.ts
+++ newWebApp/src/app/background/index/index.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IndexComponent } from './index.component';
-import {LogService} from '../../service/log.service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('BackgroundIndexComponent', () => {
