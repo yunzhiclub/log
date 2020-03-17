93d7951c02da137091a6166d118f9aa264ada90b
zhangwenda4917 <3300491785@qq.com>
2020-03-17 17:06:19 +0800

    login0317

--- newWebApp/src/app/app-routing.module.ts
+++ newWebApp/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
 
 
 const routes: Routes = [
@@ -14,6 +15,10 @@ const routes: Routes = [
   {
     path: 'user',
     loadChildren: () => import('./user/user.module').then(mod => mod.UserModule)
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
