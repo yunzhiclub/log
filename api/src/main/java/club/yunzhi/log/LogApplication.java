8871bd305dcffe9aa0a32cd1a976b0ab80fbd07c
zhangwenda4917 <3300491785@qq.com>
2020-03-18 16:57:50 +0800

    filter

--- api/src/main/java/club/yunzhi/log/LogApplication.java
+++ api/src/main/java/club/yunzhi/log/LogApplication.java
@@ -3,11 +3,13 @@ package club.yunzhi.log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync			// 启用异步
+@EnableAsync
+@ServletComponentScan// 启用异步
 @EnableScheduling		// 启用定时任务
 public class LogApplication {
 
