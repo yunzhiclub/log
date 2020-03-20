b055a531b11a97cce9936cf62dfdbc94e483a6f4
zhaokaiqiangZzz <2391945695@qq.com>
2020-03-20 15:20:35 +0800

    75

--- api/src/main/java/club/yunzhi/log/controller/UserController.java
+++ api/src/main/java/club/yunzhi/log/controller/UserController.java
@@ -3,7 +3,12 @@ package club.yunzhi.log.controller;
 import club.yunzhi.log.entity.User;
 import club.yunzhi.log.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 /**
@@ -27,5 +32,33 @@ public class UserController {
     @GetMapping("me")
     public User me(){
         return this.userService.me();
+    }
+
+    @GetMapping
+    public Page<User> getAll(@RequestParam int page,
+                             @RequestParam int size) {
+        return this.userService.findAll(PageRequest.of(page, size));
+    }
+
+    @GetMapping("{id}")
+    public User getById(@PathVariable Long id) {
+        return this.userService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User save(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @PutMapping("{id}")
+    public User update(@PathVariable Long id, @RequestBody User user) {
+        return this.userService.update(id, user);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        this.userService.deleteById(id);
     }
 }
