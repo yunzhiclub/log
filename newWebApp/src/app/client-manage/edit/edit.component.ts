979cb0360769e5e6e6a90a468461313c922d1a36
LYX6666666 <1098659745@qq.com>
2020-04-08 21:23:58 +0800

    前后台：客户端管理以及分页

--- newWebApp/src/app/client-manage/edit/edit.component.ts
+++ newWebApp/src/app/client-manage/edit/edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {Client} from '../../norm/entity/client';
+import {ClientService} from '../../service/client.service';
+import {ActivatedRoute, Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +11,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.sass']
 })
 export class EditComponent implements OnInit {
+  formGroup: FormGroup;
+  client: Client = new Client();
 
-  constructor() { }
+  constructor(private clientService: ClientService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((param: { id: number }) => {
+      this.client.id = param.id;
+      this.loadStudentById(this.client.id);
+    });
+    this.formGroup = new FormGroup({
+      name: new FormControl(''),
+      token: new FormControl(''),
+      url: new FormControl('')
+    });
+
   }
 
+  onSubmit() {
+    this.client.name = this.formGroup.get('name').value;
+    this.client.token = this.formGroup.get('token').value;
+    this.client.url = this.formGroup.get('url').value;
+    this.update(this.client);
+  }
+
+  update(client: Client) {
+    this.clientService.update(client.id, client)
+      .subscribe((result) => {
+        this.router.navigateByUrl('/client');
+        this.client = result;
+        // this.linkToIndex.nativeElement.click();
+      });
+  }
+
+  /**
+   * 加载用户
+   * @param id 用户ID
+   */
+  loadStudentById(id: number) {
+    this.clientService.getById(id)
+      .subscribe(client => {
+        this.client = client;
+        this.setFormGroupValue(this.client);
+      });
+  }
+
+  /**
+   * 设置表单值
+   * @param client 用户
+   */
+  setFormGroupValue(client: Client) {
+    this.formGroup.setValue({
+      name: client.name,
+      token: client.token,
+      url: client.url,
+    });
+  }
 }
--- newWebApp/src/app/service/client.service.ts
+++ newWebApp/src/app/service/client.service.ts
@@ -18,4 +18,37 @@ export class ClientService {
     const url = '/client/page';
     return this.httpClient.get<Page<Client>>(url, {params: Params});
   }
+
+  save(client: Client) {
+    const url = '/client';
+    return this.httpClient.post(url, client);
+  }
+
+  /**
+   * 获取某个用户
+   * @param id 用户ID
+   */
+  getById(id: number): Observable<Client> {
+    const url = `/client/${id}`;
+    return this.httpClient.get<Client>(url);
+  }
+
+  /**
+   * 更新用户
+   * @param id id
+   * @param client 用户
+   */
+  update(id: number, client: Client): Observable<Client> {
+    const url = `/client/${id}`;
+    return this.httpClient.put<Client>(url, client);
+  }
+
+  /**
+   * 删除用户
+   * @param id 用户id
+   */
+  deleteById(id: number): Observable<void> {
+    const url = `/client/${id}`;
+    return this.httpClient.delete<void>(url);
+  }
 }
