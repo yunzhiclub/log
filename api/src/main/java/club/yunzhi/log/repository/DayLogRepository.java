ab6a24f465a545eccf4142ae30391979a2fd02e4
zhangwenda4917 <3300491785@qq.com>
2020-04-08 21:01:19 +0800

    定制定时推送任务的内容：客户端，等级，等级对应日志数目

--- api/src/main/java/club/yunzhi/log/repository/DayLogRepository.java
+++ api/src/main/java/club/yunzhi/log/repository/DayLogRepository.java
@@ -2,12 +2,22 @@ package club.yunzhi.log.repository;
 
 import club.yunzhi.log.entity.DayLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 /**
  * @author jincheng
  */
-public interface DayLogRepository extends JpaRepository<DayLog , Long> {
+public interface DayLogRepository extends JpaRepository<DayLog, Long> {
+    /**
+     * 查找昨天的日志记录
+     *
+     * @return
+     */
+    @Modifying
+    @Query(value = "SELECT * FROM  day_log where  `day` <= curdate() - interval 1 day", nativeQuery = true)
+    List<DayLog> getLogOfYesterday();
 
 }
