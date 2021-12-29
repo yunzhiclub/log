9d8d6afb6dcf497507ae98d1b73cebc6f5da9267
liguowen2001 <2622498055@qq.com>
2021-12-29 21:15:31 +0800

    1

---
+++ src/app/user/add/add.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddComponent } from './add.component';
+
+/**
+ * 用户管理新增
+ */
+
+@NgModule({
+  declarations: [AddComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AddComponent
+  ]
+})
+export class AddModule { }
