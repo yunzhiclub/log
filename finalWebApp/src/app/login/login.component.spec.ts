0a67b71d81fb5060f64b1ac984521b5ada83f04e
weiweiyi189 <1589654294@qq.com>
2021-12-30 17:39:39 +0800

    test

--- finalWebApp/src/app/login/login.component.spec.ts
+++ finalWebApp/src/app/login/login.component.spec.ts
@@ -1,9 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import {LoginModule} from "./login.module";
-import {ReactiveFormsModule} from "@angular/forms";
-import {RouterTestingModule} from "@angular/router/testing";
+import {LoginModule} from './login.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
--- finalWebApp/src/app/login/login.module.ts
+++ finalWebApp/src/app/login/login.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {LoginComponent} from "./login.component";
-import {ReactiveFormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
-import {LoginRoutingModule} from "./login-routing.module";
+import {LoginComponent} from './login.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {LoginRoutingModule} from './login-routing.module';
 
 
 
