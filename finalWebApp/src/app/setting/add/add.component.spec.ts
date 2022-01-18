4697c13e264fd51b737d915fffbf23a34570433b
limingao48 <2508912994@qq.com>
2022-01-18 18:42:57 +0800

    setDing

---
+++ finalWebApp/src/app/setting/add/add.component.spec.ts
@@ -0,0 +1,37 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {AddComponent} from './add.component';
+import {getTestScheduler} from "jasmine-marbles";
+import {AddModule} from "./add.module";
+import {ClientSelectModule} from "../../log/client-select/client-select.module";
+import {ApiTestingModule} from "../../../api/api.testing.module";
+import {RouterTestingModule} from "@yunzhi/ng-router-testing";
+
+describe('AddComponent', () => {
+  let component: AddComponent;
+  let fixture: ComponentFixture<AddComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AddComponent],
+      imports: [
+        AddModule,
+        ClientSelectModule,
+        ApiTestingModule,
+        RouterTestingModule]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  fit('should create', () => {
+    expect(component).toBeTruthy();
+    getTestScheduler().flush();
+    fixture.autoDetectChanges();
+  });
+});
