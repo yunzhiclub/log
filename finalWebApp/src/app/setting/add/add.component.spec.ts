6ab714f0b1ce7adaba5b64354b6bcf3e93f90075
limingao48 <2508912994@qq.com>
2022-01-18 18:44:40 +0800

    setDing

--- finalWebApp/src/app/setting/add/add.component.spec.ts
+++ finalWebApp/src/app/setting/add/add.component.spec.ts
@@ -29,7 +29,7 @@ describe('AddComponent', () => {
     fixture.detectChanges();
   });
 
-  fit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
     getTestScheduler().flush();
     fixture.autoDetectChanges();
