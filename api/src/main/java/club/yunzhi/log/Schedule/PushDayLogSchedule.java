fe63850fda077e870c0ace07e446900fd8a278fa
limingao48 <2508912994@qq.com>
2022-02-21 09:19:10 +0800

    修改缩进

--- api/src/main/java/club/yunzhi/log/Schedule/PushDayLogSchedule.java
+++ api/src/main/java/club/yunzhi/log/Schedule/PushDayLogSchedule.java
@@ -48,37 +48,32 @@ public class PushDayLogSchedule {
 
     @Scheduled(cron = "${time.cron}")
     public void pushDayLogSchedule() throws ParseException {
-        System.out.println("执行推送任务");
-        logger.debug("首先获取所有的钉钉");
-        List<Ding> dings = dingService.getAllStartDing();
+      System.out.println("执行推送任务");
+      logger.debug("首先获取所有的钉钉");
+      List<Ding> dings = dingService.getAllStartDing();
 
-        for (Ding ding : dings) {
-            DayLog dayLog = dayLogRepository.getLogOfYesterdayWithClientId(ding.getClient().getId());
-            if (dayLog != null) {
-                this.ClientName = dayLog.getClient().getName();
-                if (dayLog.getClient().getState().equals(true)) {
-                    this.ClientState = "在线";
-                } else {
-                    this.ClientState = "离线";
-                }
-                this.LastSendTime = dayLog.getClient().getLastSendTime();
-                this.INFOCount = dayLog.getInfoCount();
-                this.ErrorCount = dayLog.getErrorCount();
-                this.WARNCount = dayLog.getWarnCount();
-                this.message = "客户端: " + this.ClientName + "\n"
-                        + "客户端状态:  " + this.ClientState + "\n"
-                        + "最后交互时间:  " + this.LastSendTime + "\n"
-                        + "昨日INFO数:   " + this.INFOCount + "\n"
-                        + "昨日ERROR数:  " + this.ErrorCount + "\n"
-                        + "昨日WARN数:   " + this.WARNCount + "\n";
-                StringBuffer resultBuffer = new StringBuffer();
-                String result = message;
-                resultBuffer.append(result);
-                String messageOfLog = resultBuffer.toString();
-                dingService.dingRequest(ding, "执行定时推送任务" + "\n" + dateString + "\n" + messageOfLog);
-                System.out.println("执行定时推送任务" + dateString + messageOfLog);
-            }
+      for (Ding ding : dings) {
+        DayLog dayLog = dayLogRepository.getLogOfYesterdayWithClientId(ding.getClient().getId());
+        if (dayLog != null) {
+          this.ClientName = dayLog.getClient().getName();
+          if (dayLog.getClient().getState().equals(true)) {
+            this.ClientState = "在线";
+          } else {
+            this.ClientState = "离线";
+          }
+          this.LastSendTime = dayLog.getClient().getLastSendTime();
+          this.INFOCount = dayLog.getInfoCount();
+          this.ErrorCount = dayLog.getErrorCount();
+          this.WARNCount = dayLog.getWarnCount();
+          this.message = "客户端: " + this.ClientName + "\n" + "客户端状态:  " + this.ClientState + "\n" + "最后交互时间:  " + this.LastSendTime + "\n" + "昨日INFO数:   " + this.INFOCount + "\n" + "昨日ERROR数:  " + this.ErrorCount + "\n" + "昨日WARN数:   " + this.WARNCount + "\n";
+          StringBuffer resultBuffer = new StringBuffer();
+          String result = message;
+          resultBuffer.append(result);
+          String messageOfLog = resultBuffer.toString();
+          dingService.dingRequest(ding, "执行定时推送任务" + "\n" + dateString + "\n" + messageOfLog);
+          System.out.println("执行定时推送任务" + dateString + messageOfLog);
         }
+      }
     }
 }
 
